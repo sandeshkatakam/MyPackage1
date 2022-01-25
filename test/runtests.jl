2fac03fb64c7b8d6f37515a91f768dc49bc78085
sandesh katakam <sandeshkatakam@gmail.com>
2022-01-25 14:56:50 +0530

    hahaha I broke the tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,10 @@
 using MyPackage1
 using Test
-@test my_f(2,1) == 7
 
 import MyPackage1
 @testset "MyPackage1.jl" begin
     @test my_f(2,1) == 7
     @test my_f(2,3) == 13
+    @test my_f(3,2) == 24
     # Write your tests here.
 end
