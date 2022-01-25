cce8f4dabb7cf99751aaca3ab12a57fce78ac5d7
sandesh katakam <sandeshkatakam@gmail.com>
2022-01-25 16:42:13 +0530

    made changes in extras and project.toml

--- src/MyPackage1.jl
+++ src/MyPackage1.jl
@@ -1,8 +1,9 @@
 module MyPackage1
-
+using ForwardDiff
 # Write your package code here.
 greet = println("hello world")
 include("extra_file.jl")
 
-export my_f
+export my_f, derivative_of_my_f
+
 end
