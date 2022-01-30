9be7178181c20ac49ab5d489924169f4c087dd61
sandesh katakam <sandeshkatakam@gmail.com>
2022-01-30 20:41:28 +0530

    added few print statements

--- src/MyPackage1.jl
+++ src/MyPackage1.jl
@@ -5,5 +5,10 @@ greet = println("hello world")
 include("extra_file.jl")
 
 export my_f,my_probability,my_sigmoid
+println("hello world")
+println("My name is sandesh")
+println("My father name is ramesh")
+println("My mother name is padma")
+println("My brother name is rishikesh")
 
 end
